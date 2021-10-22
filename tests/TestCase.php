95eb9861c611e7d206aab047f0bdf917a841b343
lupinitylabs <info@lupinitylabs.com>
2021-10-22 10:11:21 +0200

    add helper method to get the test data directory.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,8 +12,15 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    /**
+     * @var string
+     */
     protected $modelDir = '';
 
+
+    /**
+     * Set up the TestCase.
+     */
     protected function setUp(): void
     {
         parent::setUp();
@@ -21,7 +28,13 @@ class TestCase extends Orchestra
         $this->setupDatabase();
     }
 
-    protected function getPackageProviders($app)
+    /**
+     * Registers the QueryTracerServiceProvider and calls the pre-load handler.
+     *
+     * @param Application $app
+     * @return string[]
+     */
+    protected function getPackageProviders($app): array
     {
         $this->prePackageServiceProviderLoad($app);
 
@@ -30,6 +43,11 @@ class TestCase extends Orchestra
         ];
     }
 
+    /**
+     * Sets up the config and in-memory sqlite test database.
+     *
+     * @param Application $app
+     */
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'query-tracer-sqlite');
@@ -40,7 +58,11 @@ class TestCase extends Orchestra
         config()->set('query-tracer.backtrace.excludeFilesContaining', ['QueryTracer']);
     }
 
-    protected function setUpDatabase()
+
+    /**
+     * Creates the TestModel database.
+     */
+    protected function setUpDatabase(): void
     {
         $this->app->get('db')->connection()->getSchemaBuilder()
             ->create('test_models', function (Blueprint $table) {
@@ -51,24 +73,11 @@ class TestCase extends Orchestra
     }
 
     /**
-     * Copies the Models directory in some namespace under the app folder. This is for testing Model detection
-     * and initialization via the Package Service Provider and thus must be done before loading the Provider.
+     * Set the required environment before the QueryTracerServiceProvider is registered.
+     *
+     * @param Application $app
      */
-    protected function initializeModels($app)
-    {
-        putenv('QUERY_TRACER_MODEL_NAMESPACE=' . str_replace('\\', '/', Str::after($app->path(), $this->modelDir)));
-
-        $file = new Filesystem();
-
-        if (!$file->exists($this->modelDir)) {
-            $file->copyDirectory(
-                implode(DIRECTORY_SEPARATOR, [__DIR__, 'Models']),
-                $this->modelDir
-            );
-        }
-    }
-
-    protected function prePackageServiceProviderLoad(Application $app)
+    protected function prePackageServiceProviderLoad(Application $app): void
     {
         // Make app()->environment return whatever we set in APP_ENV.
         $app->detectEnvironment(function () {
@@ -83,5 +92,34 @@ class TestCase extends Orchestra
         $this->modelDir = $app->path('Some/Obscure/Path/To/Models');
 
         $this->initializeModels($app);
+    }
+
+    /**
+     * Copies the Models directory in some namespace under the app folder. This is for testing Model detection
+     * and initialization via the Package Service Provider and thus must be done before loading the Provider.
+     */
+    protected function initializeModels($app): void
+    {
+        putenv('QUERY_TRACER_MODEL_NAMESPACE=' . str_replace('\\', '/', Str::after($app->path(), $this->modelDir)));
+
+        $file = new Filesystem();
+
+        if (!$file->exists($this->modelDir)) {
+            $file->copyDirectory(
+                $this->getDataDir('Models'),
+                $this->modelDir
+            );
+        }
+    }
+
+    /**
+     * Helper method to return the test data directory.
+     *
+     * @param string $directory
+     * @return string
+     */
+    public function getDataDir(string $target = ''): string
+    {
+        return implode(DIRECTORY_SEPARATOR, [__DIR__, 'Data', $target]);
     }
 }
